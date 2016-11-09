351c7545b359c70ca8b590792e2cae38d49bb0db
rryqszq4 <279664262@163.com>
2016-11-09 19:55:26 +0800

    [src] change code py to python

--- src/ngx_http_python_module.c
+++ src/ngx_http_python_module.c
@@ -4,93 +4,93 @@
  *
  */
 
+#include <Python.h>
+
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
 #include <ngx_conf_file.h>
 #include <nginx.h>
 
-#include <Python.h>
+#include "ngx_http_python_module.h"
 
-#include "ngx_http_py_module.h"
+static ngx_int_t ngx_http_python_init(ngx_conf_t *cf);
+static ngx_int_t ngx_http_python_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_t *pmcf);
 
-static ngx_int_t ngx_http_py_init(ngx_conf_t *cf);
-static ngx_int_t ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_t *pmcf);
+static void *ngx_http_python_create_main_conf(ngx_conf_t *cf);
+static char *ngx_http_python_init_main_conf(ngx_conf_t *cf, void *conf);
 
-static void *ngx_http_py_create_main_conf(ngx_conf_t *cf);
-static char *ngx_http_py_init_main_conf(ngx_conf_t *cf, void *conf);
+static void *ngx_http_python_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_python_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
 
-static void *ngx_http_py_create_loc_conf(ngx_conf_t *cf);
-static char *ngx_http_py_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static ngx_int_t ngx_http_python_init_worker(ngx_cycle_t *cycle);
+static void ngx_http_python_exit_worker(ngx_cycle_t *cycle);
 
-static ngx_int_t ngx_http_py_init_worker(ngx_cycle_t *cycle);
-static void ngx_http_py_exit_worker(ngx_cycle_t *cycle);
+static ngx_command_t ngx_http_python_commands[] = {
 
-static ngx_command_t ngx_http_py_commands[] = {
-
-    {ngx_string("content_by_py_file"),
+    {ngx_string("content_by_python_file"),
      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF
         |NGX_CONF_TAKE1,
-     ngx_http_py_content_phase,
+     ngx_http_python_content_phase,
      NGX_HTTP_LOC_CONF_OFFSET,
      0,
      ngx_http_py_content_file_handler
     },
 
-    {ngx_string("content_by_py"),
+    {ngx_string("content_by_python"),
      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF
         |NGX_CONF_TAKE1,
-     ngx_http_py_content_inline_phase,
+     ngx_http_python_content_inline_phase,
      NGX_HTTP_LOC_CONF_OFFSET,
      0,
-     ngx_http_py_content_inline_handler
+     ngx_http_python_content_inline_handler
     },
 
     ngx_null_command
 
 };
 
-static ngx_http_module_t ngx_http_py_module_ctx = {
+static ngx_http_module_t ngx_http_python_module_ctx = {
     NULL,                           /* preconfiguration */
-    ngx_http_py_init,               /* postconfiguration */
+    ngx_http_python_init,               /* postconfiguration */
 
-    ngx_http_py_create_main_conf,   /* create main configuration */
-    ngx_http_py_init_main_conf,     /* init main configuration */
+    ngx_http_python_create_main_conf,   /* create main configuration */
+    ngx_http_python_init_main_conf,     /* init main configuration */
 
     NULL,                           /* create server configuration */
     NULL,                           /* merge server configuration */
 
-    ngx_http_py_create_main_conf,   /* create location configuration */
-    ngx_http_py_merge_loc_conf      /* merge location configuration */
+    ngx_http_python_create_main_conf,   /* create location configuration */
+    ngx_http_python_merge_loc_conf      /* merge location configuration */
 };
 
-ngx_module_t ngx_http_py_module = {
+ngx_module_t ngx_http_python_module = {
     NGX_MODULE_V1,
-    &ngx_http_py_module_ctx,    /* module context */
-    ngx_http_py_commands,       /* module directives */
+    &ngx_http_python_module_ctx,    /* module context */
+    ngx_http_python_commands,       /* module directives */
     NGX_HTTP_MODULE,               /* module type */
     NULL,                          /* init master */
     NULL,                          /* init module */
-    ngx_http_py_init_worker,      /* init process */
+    ngx_http_python_init_worker,      /* init process */
     NULL,                          /* init thread */
     NULL,                          /* exit thread */
-    ngx_http_py_exit_worker,      /* exit process */
+    ngx_http_python_exit_worker,      /* exit process */
     NULL,                          /* exit master */
     NGX_MODULE_V1_PADDING
 };
 
 static ngx_int_t
-ngx_http_py_init(ngx_conf_t *cf)
+ngx_http_python_init(ngx_conf_t *cf)
 {
     ngx_http_core_main_conf_t *cmcf;
-    ngx_http_py_main_conf_t *pmcf;
+    ngx_http_python_main_conf_t *pmcf;
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-    pmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_py_module);
+    pmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_python_module);
 
-    ngx_py_request = NULL;
+    ngx_python_request = NULL;
 
-    if (ngx_http_py_handler_init(cmcf, pmcf) != NGX_OK){
+    if (ngx_http_python_handler_init(cmcf, pmcf) != NGX_OK){
         return NGX_ERROR;
     }
 
@@ -98,7 +98,7 @@ ngx_http_py_init(ngx_conf_t *cf)
 }
 
 static ngx_int_t
-ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_t *pmcf)
+ngx_http_python_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_t *pmcf)
 {
     ngx_int_t i;
     ngx_http_handler_pt *h;
@@ -121,7 +121,7 @@ ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_
                     if (h == NULL){
                         return NGX_ERROR;
                     }
-                    *h = ngx_http_py_post_read_handler;
+                    *h = ngx_http_python_post_read_handler;
                 }
                 break;
             case NGX_HTTP_REWRITE_PHASE:
@@ -130,7 +130,7 @@ ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_
                     if (h == NULL){
                         return NGX_ERROR;
                     }
-                    *h = ngx_http_py_rewrite_handler;
+                    *h = ngx_http_python_rewrite_handler;
                 }
                 break;
             case NGX_HTTP_ACCESS_PHASE:
@@ -139,7 +139,7 @@ ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_
                     if (h == NULL){
                         return NGX_ERROR;
                     }
-                    *h = ngx_http_py_access_handler;
+                    *h = ngx_http_python_access_handler;
                 }
                 break;*/
             case NGX_HTTP_CONTENT_PHASE:
@@ -148,7 +148,7 @@ ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_
                     if (h == NULL){
                         return NGX_ERROR;
                     }
-                    *h = ngx_http_py_content_handler;
+                    *h = ngx_http_python_content_handler;
                 }
                 break;
             default:
@@ -160,16 +160,16 @@ ngx_http_py_handler_init(ngx_http_core_main_conf_t *cmcf, ngx_http_py_main_conf_
 }
 
 static void *
-ngx_http_py_create_main_conf(ngx_conf_t *cf)
+ngx_http_python_create_main_conf(ngx_conf_t *cf)
 {
-    ngx_http_py_main_conf_t *pmcf;
+    ngx_http_python_main_conf_t *pmcf;
 
-    pmcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_py_main_conf_t));
+    pmcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_python_main_conf_t));
     if (pmcf == NULL){
         return NULL;
     }
 
-    /*pmcf->state = ngx_pcalloc(cf->pool, sizeof(ngx_http_py_state_t));
+    /*pmcf->state = ngx_pcalloc(cf->pool, sizeof(ngx_http_python_state_t));
     if (pmcf->state == NULL){
         return NULL;
     }*/
@@ -179,17 +179,17 @@ ngx_http_py_create_main_conf(ngx_conf_t *cf)
 }
 
 static char *
-ngx_http_py_init_main_conf(ngx_conf_t *cf, void *conf)
+ngx_http_python_init_main_conf(ngx_conf_t *cf, void *conf)
 {
     return NGX_CONF_OK;
 }
 
 static void *
-ngx_http_py_create_loc_conf(ngx_conf_t *cf)
+ngx_http_python_create_loc_conf(ngx_conf_t *cf)
 {
-    ngx_http_py_loc_conf_t *plcf;
+    ngx_http_python_loc_conf_t *plcf;
 
-    plcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_py_loc_conf_t));
+    plcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_python_loc_conf_t));
     if (plcf == NULL){
         return NGX_CONF_ERROR;
     }
@@ -207,13 +207,13 @@ ngx_http_py_create_loc_conf(ngx_conf_t *cf)
 }
 
 static char *
-ngx_http_py_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+ngx_http_python_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 {
     ngx_http_core_loc_conf_t  *clcf;
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
 
-    ngx_http_py_loc_conf_t *prev = parent;
-    ngx_http_py_loc_conf_t *conf = child;
+    ngx_http_python_loc_conf_t *prev = parent;
+    ngx_http_python_loc_conf_t *conf = child;
 
     //prev->rewrite_code = conf->rewrite_code;
     //prev->rewrite_inline_code = conf->rewrite_inline_code;
@@ -228,11 +228,11 @@ ngx_http_py_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 }
 
 static ngx_int_t
-ngx_http_py_init_worker(ngx_cycle_t *cycle)
+ngx_http_python_init_worker(ngx_cycle_t *cycle)
 {
-    ngx_http_py_main_conf_t *pmcf;
+    ngx_http_python_main_conf_t *pmcf;
 
-    pmcf = ngx_http_cycle_get_module_main_conf(cycle, ngx_http_py_module);
+    pmcf = ngx_http_cycle_get_module_main_conf(cycle, ngx_http_python_module);
 
     Py_Initialize();
 
@@ -240,7 +240,7 @@ ngx_http_py_init_worker(ngx_cycle_t *cycle)
 }
 
 static void 
-ngx_http_py_exit_worker(ngx_cycle_t *cycle)
+ngx_http_python_exit_worker(ngx_cycle_t *cycle)
 {
     Py_Finalize();
 }
---
+++ src/ngx_http_python_module.h
@@ -0,0 +1,30 @@
+/**
+ *    Copyright(c) 2016 rryqszq4
+ *
+ *
+ */
+
+#ifndef NGX_HTTP_PYTHON_MODULE_H
+#define NGX_HTTP_PYTHON_MODULE_H
+
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <ngx_config.h>
+#include <nginx.h>
+
+#define MODULE_NAME "ngx_python"
+#define MODULE_VERSION "0.0.1"
+
+extern ngx_module_t ngx_http_python_module;
+ngx_http_request_t *ngx_python_request;
+
+typedef struct {
+    unsigned enable_content_handler:1;
+} ngx_http_python_main_conf_t;
+
+typedef struct {
+    ngx_http_python_code_t *content_file;
+    ngx_http_python_code_t *content_inline_code;
+} ngx_http_python_loc_conf_t;
+
+#endif
