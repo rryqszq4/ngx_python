86a638e5a5d66a10fb8f4974b1c5a0152e7433bb
rryqszq4 <279664262@163.com>
2017-04-25 19:16:53 +0800

    [src] Update content handler.

--- src/ngx_http_python_handler.c
+++ src/ngx_http_python_handler.c
@@ -343,6 +343,8 @@ ngx_http_python_content_file_handler(ngx_http_request_t *r)
     }
     ngx_http_set_ctx(r, ctx, ngx_http_python_module);
 
+    ctx->exit_code = NGX_OK;
+
     ngx_python_request = r;
 
     FILE * fp = fopen(plcf->content_file_code->code.file, "r");        
@@ -352,62 +354,72 @@ ngx_http_python_content_file_handler(ngx_http_request_t *r)
     ngx_http_python_rputs_chain_list_t *chain;
     
     ctx = ngx_http_get_module_ctx(r, ngx_http_python_module);
-    chain = ctx->rputs_chain;
-    
-    if (ctx->rputs_chain == NULL){
-        ngx_buf_t *b;
-        ngx_str_t ns;
-        u_char *u_str;
-        ns.data = (u_char *)" ";
-        ns.len = 1;
+    rc = ctx->exit_code;
+
+    if (rc == NGX_OK || rc == NGX_HTTP_OK) {
+        chain = ctx->rputs_chain;
         
-        chain = ngx_pcalloc(r->pool, sizeof(ngx_http_python_rputs_chain_list_t));
-        chain->out = ngx_alloc_chain_link(r->pool);
-        chain->last = &chain->out;
-    
-        b = ngx_calloc_buf(r->pool);
-        (*chain->last)->buf = b;
-        (*chain->last)->next = NULL;
+        if (ctx->rputs_chain == NULL){
+            ngx_buf_t *b;
+            ngx_str_t ns;
+            u_char *u_str;
+            ns.data = (u_char *)" ";
+            ns.len = 1;
+            
+            chain = ngx_pcalloc(r->pool, sizeof(ngx_http_python_rputs_chain_list_t));
+            chain->out = ngx_alloc_chain_link(r->pool);
+            chain->last = &chain->out;
+        
+            b = ngx_calloc_buf(r->pool);
+            (*chain->last)->buf = b;
+            (*chain->last)->next = NULL;
 
-        u_str = ngx_pstrdup(r->pool, &ns);
-        //u_str[ns.len] = '\0';
-        (*chain->last)->buf->pos = u_str;
-        (*chain->last)->buf->last = u_str + ns.len;
-        (*chain->last)->buf->memory = 1;
-        ctx->rputs_chain = chain;
+            u_str = ngx_pstrdup(r->pool, &ns);
+            //u_str[ns.len] = '\0';
+            (*chain->last)->buf->pos = u_str;
+            (*chain->last)->buf->last = u_str + ns.len;
+            (*chain->last)->buf->memory = 1;
+            ctx->rputs_chain = chain;
 
-        if (r->headers_out.content_length_n == -1){
-            r->headers_out.content_length_n += ns.len + 1;
-        }else {
-            r->headers_out.content_length_n += ns.len;
+            if (r->headers_out.content_length_n == -1){
+                r->headers_out.content_length_n += ns.len + 1;
+            }else {
+                r->headers_out.content_length_n += ns.len;
+            }
         }
-    }
 
-    //r->headers_out.content_type.len = sizeof("text/html") - 1;
-    //r->headers_out.content_type.data = (u_char *)"text/html";
-    if (!r->headers_out.status){
-        r->headers_out.status = NGX_HTTP_OK;
-    }
+        //r->headers_out.content_type.len = sizeof("text/html") - 1;
+        //r->headers_out.content_type.data = (u_char *)"text/html";
+        if (!r->headers_out.status){
+            r->headers_out.status = NGX_HTTP_OK;
+        }
 
-    if (r->method == NGX_HTTP_HEAD){
+        if (r->method == NGX_HTTP_HEAD){
+            rc = ngx_http_send_header(r);
+            if (rc != NGX_OK){
+                return rc;
+            }
+        }
+
+        if (chain != NULL){
+            (*chain->last)->buf->last_buf = 1;
+        }
+
         rc = ngx_http_send_header(r);
         if (rc != NGX_OK){
             return rc;
         }
+
+        ngx_http_output_filter(r, chain->out);
+
+        ngx_http_set_ctx(r, NULL, ngx_http_python_module);
+
+        return NGX_OK;
     }
 
-    if (chain != NULL){
-        (*chain->last)->buf->last_buf = 1;
-    }
-
-    rc = ngx_http_send_header(r);
-    if (rc != NGX_OK){
+    if (rc == NGX_ERROR || rc > NGX_OK) {
         return rc;
     }
-
-    ngx_http_output_filter(r, chain->out);
-
-    ngx_http_set_ctx(r, NULL, ngx_http_python_module);
 
     return NGX_OK;
 }
@@ -433,6 +445,8 @@ ngx_http_python_content_inline_handler(ngx_http_request_t *r)
     ctx->request_body_more = 1;
     ngx_http_set_ctx(r, ctx, ngx_http_python_module);
 
+    ctx->exit_code = NGX_OK;
+
     ngx_python_request = r;
 
     PyRun_SimpleString(plcf->content_inline_code->code.string);
@@ -440,62 +454,75 @@ ngx_http_python_content_inline_handler(ngx_http_request_t *r)
     ngx_http_python_rputs_chain_list_t *chain;
     
     ctx = ngx_http_get_module_ctx(r, ngx_http_python_module);
-    chain = ctx->rputs_chain;
+    rc = ctx->exit_code;
 
-    if (ctx->rputs_chain == NULL){
-        ngx_buf_t *b;
-        ngx_str_t ns;
-        u_char *u_str;
-        ns.data = (u_char *)" ";
-        ns.len = 1;
+    if (rc == NGX_OK || rc == NGX_HTTP_OK) {
+        chain = ctx->rputs_chain;
+
+        if (ctx->rputs_chain == NULL){
+            ngx_buf_t *b;
+            ngx_str_t ns;
+            u_char *u_str;
+            ns.data = (u_char *)" ";
+            ns.len = 1;
+            
+            chain = ngx_pcalloc(r->pool, sizeof(ngx_http_python_rputs_chain_list_t));
+            chain->out = ngx_alloc_chain_link(r->pool);
+            chain->last = &chain->out;
         
-        chain = ngx_pcalloc(r->pool, sizeof(ngx_http_python_rputs_chain_list_t));
-        chain->out = ngx_alloc_chain_link(r->pool);
-        chain->last = &chain->out;
-    
-        b = ngx_calloc_buf(r->pool);
-        (*chain->last)->buf = b;
-        (*chain->last)->next = NULL;
+            b = ngx_calloc_buf(r->pool);
+            (*chain->last)->buf = b;
+            (*chain->last)->next = NULL;
 
-        u_str = ngx_pstrdup(r->pool, &ns);
-        //u_str[ns.len] = '\0';
-        (*chain->last)->buf->pos = u_str;
-        (*chain->last)->buf->last = u_str + ns.len;
-        (*chain->last)->buf->memory = 1;
-        ctx->rputs_chain = chain;
+            u_str = ngx_pstrdup(r->pool, &ns);
+            //u_str[ns.len] = '\0';
+            (*chain->last)->buf->pos = u_str;
+            (*chain->last)->buf->last = u_str + ns.len;
+            (*chain->last)->buf->memory = 1;
+            ctx->rputs_chain = chain;
 
-        if (r->headers_out.content_length_n == -1){
-            r->headers_out.content_length_n += ns.len + 1;
-        }else {
-            r->headers_out.content_length_n += ns.len;
+            if (r->headers_out.content_length_n == -1){
+                r->headers_out.content_length_n += ns.len + 1;
+            }else {
+                r->headers_out.content_length_n += ns.len;
+            }
         }
-    }
 
-    //r->headers_out.content_type.len = sizeof("text/html") - 1;
-    //r->headers_out.content_type.data = (u_char *)"text/html";
-    if (!r->headers_out.status){
-        r->headers_out.status = NGX_HTTP_OK;
-    }
+        //r->headers_out.content_type.len = sizeof("text/html") - 1;
+        //r->headers_out.content_type.data = (u_char *)"text/html";
+        if (!r->headers_out.status){
+            r->headers_out.status = NGX_HTTP_OK;
+        }
 
-    if (r->method == NGX_HTTP_HEAD){
+        if (r->method == NGX_HTTP_HEAD){
+            rc = ngx_http_send_header(r);
+            if (rc != NGX_OK){
+                return rc;
+            }
+        }
+
+        if (chain != NULL){
+            (*chain->last)->buf->last_buf = 1;
+        }
+
         rc = ngx_http_send_header(r);
         if (rc != NGX_OK){
             return rc;
         }
+
+        ngx_http_output_filter(r, chain->out);
+
+        ngx_http_set_ctx(r, NULL, ngx_http_python_module);
+
+        return NGX_OK;
     }
 
-    if (chain != NULL){
-        (*chain->last)->buf->last_buf = 1;
-    }
-
-    rc = ngx_http_send_header(r);
-    if (rc != NGX_OK){
+    if (rc == NGX_ERROR || rc > NGX_OK) {
         return rc;
     }
 
-    ngx_http_output_filter(r, chain->out);
-
-    ngx_http_set_ctx(r, NULL, ngx_http_python_module);
-
     return NGX_OK;
 }
+
+
+
