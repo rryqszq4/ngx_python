52bbf5321217bcb6ed6cf86c3c07192e679c37e7
quanzhao <279664262@163.com>
2017-11-01 21:49:38 +0800

    [src] Add ext python_ngx_request .

---
+++ src/python/python_ngx_request.c
@@ -0,0 +1,10 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#include <Python.h>
+#include "python_ngx_request.h"
+#include "../ngx_http_python_module.h"
+#include "../ngx_http_python_code.h"
---
+++ src/python/python_ngx_request.h
@@ -0,0 +1,12 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#ifndef PYTHON_NGX_REQUEST_H
+#define PYTHON_NGX_REQUEST_H
+
+void initNgxRequest();
+
+#endif
