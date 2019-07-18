e9cf614423380c68abbba2fb35725e6c87976cc3
Quan Zhao <quanzhao@sohu-inc.com>
2019-07-18 17:43:11 +0800

    Create ngx_http_python_util

---
+++ src/ngx_http_python_util.h
@@ -0,0 +1,13 @@
+/**
+ *    Copyright(c) 2016-2019 rryqszq4
+ */
+
+#ifndef NGX_HTTP_PYTHON_UTIL_H
+#define NGX_HTTP_PYTHON_UTIL_H
+
+#include <nginx.h>
+#include <ngx_http.h>
+
+char *str_replace(const char *str, const char *sub, const char *replace);
+
+#endif
