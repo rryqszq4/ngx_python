fc79c9f539455076ec71669a39a52cafbf0a0c4a
Quan Zhao <quanzhao@sohu-inc.com>
2019-07-22 17:33:50 +0800

    Update ngx_http_python_fnthread.c

--- src/ngx_http_python_fnthread.c
+++ src/ngx_http_python_fnthread.c
@@ -46,7 +46,7 @@ ngx_http_python_fnthread_content_inline_routine(ngx_http_request_t *r)
 	tmp_str = str_replace((char *)plcf->content_inline_code->code.string, "\t", "");
 	tmp_str2 = str_replace(tmp_str, "\n", "\n    ");
 
-	inline_code.data = ngx_pnalloc(r->pool, sizeof("def ngx_content_():")-1 + ngx_strlen(tmp_str2) + 32);
+	inline_code.data = (u_char *)malloc(sizeof("def ngx_content_():")-1 + ngx_strlen(tmp_str2) + 32);
 
 	inline_code.len = ngx_sprintf(inline_code.data, "def ngx_content_%V():\n%*s", 
                                         &(plcf->content_inline_code->code_id), 
@@ -57,7 +57,8 @@ ngx_http_python_fnthread_content_inline_routine(ngx_http_request_t *r)
 	if (!plcf->enabled_content_inline_compile) {
 		PyRun_SimpleString((char *)inline_code.data);
 		plcf->enabled_content_inline_compile = 1;
-		printf("%s\n", inline_code.data);
+		printf("%s, %d\n", inline_code.data, (int)strlen((char *)inline_code.data));
+		free(inline_code.data);
 	}
 
 	ngx_http_python_fnthread_create(r, "ngx_content");
@@ -72,14 +73,17 @@ ngx_http_python_fnthread_create(ngx_http_request_t *r, char *func_prefix)
 
 	plcf = ngx_http_get_module_loc_conf(r, ngx_http_python_module);
 
-	char *f_name = malloc(strlen(func_prefix) + 32);
+	char *f_name = malloc(strlen(func_prefix)+1 + 32);
 	if (strcmp(func_prefix, "ngx_content") == 0) {
-        func_name.len = ngx_sprintf((u_char *)f_name, "%s_%V", func_prefix, &(plcf->content_inline_code->code_id)) - func_name.data;
+        //func_name.len = ngx_sprintf((u_char *)f_name, "%s_%V", func_prefix, &(plcf->content_inline_code->code_id)) - func_name.data;
+        strncat(f_name, func_prefix, strlen(func_prefix));
+        strncat(f_name, "_", 1);
+        strncat(f_name, (char *)plcf->content_inline_code->code_id.data, 32);
     }else {
         func_name.len = 0;
     }
 
-    printf("%s, %d\n", (char *)f_name, (int)func_name.len);
+    printf("%s, %d\n", (char *)f_name, (int)strlen((char *)f_name));
 
     fn_name = PyUnicode_FromString("__main__");
 	module_name = PyImport_Import(fn_name);
